5488fe818ec6c9b94a7253ef9f7ef3561081b0ab
khairunisaadabb <khairunisa.adabbia@gmail.com>
2020-10-07 19:42:03 +0700

    commit loading splash2 bia

--- app/src/main/java/android/example/helloworld/SplashActivity.java
+++ app/src/main/java/android/example/helloworld/SplashActivity.java
@@ -6,18 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        int SPLASH_TIME = 3000;
+        progressBar =findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.text_view);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+        progressAnimation();
+
+
+        int SPLASH_TIME = 9000;
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -27,5 +40,11 @@ public class SplashActivity extends AppCompatActivity {
                 SplashActivity.this.finish();
             }
         }, SPLASH_TIME);
+    }
+
+    public void progressAnimation () {
+        ProgressBarAnimation anim = new ProgressBarAnimation(this, progressBar, textView, 0f, 100f);
+        anim.setDuration(8000);
+        progressBar.setAnimation(anim);
     }
 }
